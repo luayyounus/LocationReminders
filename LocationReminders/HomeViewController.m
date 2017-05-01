dda342367c3ae4094e575ffecc2d64db1b125a19
Luay Younus <luayghazwan@yahoo.com>
2017-05-01 15:13:06 -0700

    Created New buttons to show different places on the map.

--- LocationReminders/HomeViewController.m
+++ LocationReminders/HomeViewController.m
@@ -1,24 +1,24 @@
 //
-//  ViewController.m
+//  HomeViewController.m
 //  LocationReminders
 //
 //  Created by Luay Younus on 5/1/17.
 //  Copyright © 2017 Luay Younus. All rights reserved.
 //
 
-#import "ViewController.h"
+#import "HomeViewController.h"
 
 @import Parse;
 @import MapKit;
 
-@interface ViewController ()
+@interface HomeViewController ()
 
 @property(weak, nonatomic) IBOutlet MKMapView *mapView;
 @property(strong,nonatomic) CLLocationManager *locationManager;
 
 @end
 
-@implementation ViewController
+@implementation HomeViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
@@ -44,5 +44,21 @@
     
     [self.mapView setRegion:region animated:YES];
 }
+- (IBAction)codeFellowsPressed:(id)sender {
+    CLLocationCoordinate2D coordinate = CLLocationCoordinate2DMake(47.6182169,-122.3529265);
+    
+    MKCoordinateRegion region = MKCoordinateRegionMakeWithDistance(coordinate, 500.0, 500.0);
+    
+    [self.mapView setRegion:region animated:YES];
+    
+}
+- (IBAction)canlisLocationPressed:(id)sender {
+    CLLocationCoordinate2D coordinate = CLLocationCoordinate2DMake(47.6430933,-122.3489422);
+    
+    MKCoordinateRegion region = MKCoordinateRegionMakeWithDistance(coordinate, 500.0, 500.);
+    
+    [self.mapView setRegion:region animated:YES];
+}
+
 
 @end

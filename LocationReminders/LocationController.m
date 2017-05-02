a0116e313fed7da106317091c56ecbefe9c0c4a2
Luay Younus <luayghazwan@yahoo.com>
2017-05-02 16:33:17 -0700

    Singleton Creation with the methods required that belong to The Location Controller. Created Protocol for ControllerUpdatedLocation for the HomeViewControl to conform to.

---
+++ LocationReminders/LocationController.m
@@ -0,0 +1,51 @@
+//
+//  LocationController.m
+//  LocationReminders
+//
+//  Created by Luay Younus on 5/2/17.
+//  Copyright © 2017 Luay Younus. All rights reserved.
+//
+
+#import "LocationController.h"
+#import "HomeViewController.h"
+
+@interface LocationController()<CLLocationManagerDelegate>
+
+@end
+
+@implementation LocationController
+
++(instancetype)shared{
+    static LocationController *shared = nil;
+    
+    static dispatch_once_t onceToken;
+    dispatch_once(&onceToken, ^{
+        shared = [[[self class]alloc]init];
+    });
+    return shared;
+}
+
+-(void)requestPermission{
+    self.locationManager = [[CLLocationManager alloc]init];
+    self.locationManager.desiredAccuracy = kCLLocationAccuracyBest;
+    self.locationManager.distanceFilter = 100; //meters
+    
+    self.locationManager.delegate = self;
+    [self.locationManager requestAlwaysAuthorization];
+    [self.locationManager startUpdatingLocation];
+}
+
+-(void)locationManager:(CLLocationManager *)manager didUpdateLocations:(NSArray<CLLocation *> *)locations{
+    CLLocation *location = locations.lastObject;
+    
+    [self.delegate locationControllerUpdatedLocation:location];
+}
+
+-(void)locationManager:(CLLocationManager *)manager didFailWithError:(NSError *)error{
+    NSLog(@"The location is not accurate");
+}
+
+
+
+
+@end

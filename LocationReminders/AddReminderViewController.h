7e706f707fc620ce691c83c5d7b5b6948d5dda9c
Luay Younus <luayghazwan@yahoo.com>
2017-05-02 10:57:04 -0700

    Added ReminderViewController Created segue from the HomeViewController

---
+++ LocationReminders/AddReminderViewController.h
@@ -0,0 +1,13 @@
+//
+//  AddReminderViewController.h
+//  LocationReminders
+//
+//  Created by Luay Younus on 5/2/17.
+//  Copyright © 2017 Luay Younus. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AddReminderViewController : UIViewController
+
+@end
---
+++ LocationReminders/AddReminderViewController.m
@@ -0,0 +1,21 @@
+//
+//  AddReminderViewController.m
+//  LocationReminders
+//
+//  Created by Luay Younus on 5/2/17.
+//  Copyright © 2017 Luay Younus. All rights reserved.
+//
+
+#import "AddReminderViewController.h"
+
+@interface AddReminderViewController ()
+
+@end
+
+@implementation AddReminderViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+}
+
+@end
